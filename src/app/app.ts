98b58ff6ff5dacb172d7a776a5116919ef261080
Haneul Jin <hnjin@citus.co.kr>
2026-02-27 08:57:45 +0900

    Update project configuration: add 'dist' to .gitignore, modify package.json scripts for build process, and adjust import statements to include file extensions.

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,5 +1,5 @@
 import express from "express";
-import { PORT } from "./const";
+import { PORT } from "./const.js";
 
 const app = express();
 
--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,4 @@
-import app from "./app/app";
+import app from "./app/app.js";
 import { VisionLabelsReq } from "./types";
 
 app.post(
