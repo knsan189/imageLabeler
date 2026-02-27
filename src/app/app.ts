de9fbda6e86d7efd67f1d2a9bc9c96387eb1d378
Haneul Jin <hnjin@citus.co.kr>
2026-02-27 09:00:38 +0900

    Update server listening address to allow external connections by changing from localhost to 0.0.0.0.

--- src/app/app.ts
+++ src/app/app.ts
@@ -6,7 +6,7 @@ const app = express();
 app.use(express.json({ limit: "50mb" }));
 app.use(express.urlencoded({ extended: true }));
 
-app.listen(PORT, () => {
+app.listen(PORT, "0.0.0.0", () => {
   console.log(`Server running on http://localhost:${PORT}`);
 });
 export default app;
